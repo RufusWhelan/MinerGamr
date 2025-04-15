417c074bf8289317a87ff3a3e9a8db6632fed802
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2025-04-15 11:05:23 +1000

    Initial commit

---
+++ Assets/scripts/playerMovementData.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+public class playerMovementData : MonoBehaviour
+{
+    [Header("Run")]
+    public float rawAccelaration;
+    [HideInInspector] public float accelAmount;
+    public float rawDeccelaration;
+    [HideInInspector] public float deccelAmount;
+    public int topSpeed;
+
+    [HideInInspector] public bool grounded = false;
+    [HideInInspector] public float groundCheckDistance = 0.5f;
+    [HideInInspector] public RaycastHit hit;
+
+    [HideInInspector]public float jumps;
+    [HideInInspector]public bool jumpState = false;
+
+    [Space(5)]
+    [Header("Jump")]
+    public float jumpHeight;
+    public float timeToApex;
+    [HideInInspector] public float jumpForce;
+    public float jumpHangTimeThreshold;
+    public float jumpHangTimeMulti;
+    public float jumpCutGravMulti;
+
+    [HideInInspector]public float coyoteTime;
+
+    [Space(5)]
+    [Header("CoyoteTime")]
+    public float coyoteTimeCounter;
+    [HideInInspector]public float jumpBuffer;
+    public float jumpBufferCounter;
+
+    [Space(5)]
+    [Header("Gravity")]
+    public float gravityStrength;
+    private void OnValidate()
+    {
+        accelAmount = (rawAccelaration * 50) / topSpeed; //makes it so that accelaration is applied
+        deccelAmount = (rawDeccelaration * 50) / topSpeed;
+
+        gravityStrength = -(2 * jumpHeight) / (timeToApex * timeToApex); //a physics formula which established what the strength of gravity should be to allow the player to jump an entered height
+        jumpForce = Mathf.Abs(gravityStrength) * timeToApex; //determins the amount of force required for the jump to reach the specified height that the specified rate
+
+    }
+}
---
+++ Assets/scripts/playerMovementScript.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class playerMovementScript : MonoBehaviour
+{
+    public playerMovementData Data;
+    [SerializeField] private Rigidbody playerBody;
+    private Vector3 playerMovementInput;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); //gets the users WASD input and turns it into a useable vector
+        if (Input.GetKeyDown(KeyCode.Space) && Data.coyoteTime > 0 && playerBody.linearVelocity.y <= 0.05f)
+        {
+            Data.jumpState = true;   
+        }
+    }
+    void FixedUpdate()
+    {
+        groundCheck();
+        gravity();
+        MovePlayer();
+        if (Data.jumpState == true)
+        {
+            Jump();
+            Data.coyoteTime = 0;
+            Data.jumpState = false;
+        }
+    }
+
+    private void groundCheck()
+    {  
+        if (Physics.Raycast(transform.position, -transform.up, out Data.hit, Data.groundCheckDistance))
+            Data.coyoteTime = Data.coyoteTimeCounter;
+        else
+            Data.coyoteTime -= Time.deltaTime;
+    }
+    private void gravity()
+    {
+        playerBody.AddForce(Vector3.up * Data.gravityStrength, ForceMode.Acceleration);
+        Vector3 velocity = playerBody.linearVelocity;
+        velocity.y = Mathf.Clamp(velocity.y, -40f, float.MaxValue);
+        playerBody.linearVelocity = velocity;
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 moveVector = transform.TransformDirection(playerMovementInput); //makes the WASD vector relative to the direction the player is facing
+        Vector3 targetSpeed = new Vector3(moveVector.x * Data.topSpeed, 0f, moveVector.z * Data.topSpeed); //ensures the the users target speed as the correct sign.
+
+        Vector3 accelRate;
+        accelRate = new Vector3(Mathf.Abs(targetSpeed.x) > 0.01 ? Data.accelAmount : Data.deccelAmount, 0f,  Mathf.Abs(targetSpeed.z) > 0.01 ? Data.accelAmount : Data.deccelAmount); //checks if the player is actively moving and applies accelarion or deccelaration accordingly
+
+        if (Mathf.Abs(playerBody.linearVelocity.x) > Mathf.Abs(targetSpeed.x) && Mathf.Abs(playerBody.linearVelocity.x) == Mathf.Abs(targetSpeed.x) && Mathf.Abs(targetSpeed.x) > 0.01f)
+            accelRate.x = 0;
+        
+        if (Mathf.Abs(playerBody.linearVelocity.z) > Mathf.Abs(targetSpeed.z) && Mathf.Abs(playerBody.linearVelocity.z) == Mathf.Abs(targetSpeed.z) && Mathf.Abs(targetSpeed.z) > 0.01f)
+            accelRate.z = 0;
+        //allows the player to conserve momentum even if they are surpassing the target speed as long as they don't slow down
+
+        Vector3 speedDif = new Vector3(targetSpeed.x - playerBody.linearVelocity.x, playerBody.linearVelocity.y, targetSpeed.z - playerBody.linearVelocity.z); //compares players current speed to target speed
+        Vector3 movement;
+        if (moveVector.x != 0 && moveVector.z != 0) //checks if the player is moving diagonally
+        {
+            movement = new Vector3((speedDif.x * accelRate.x) / Mathf.Sqrt(2), 0f, (speedDif.z * accelRate.z) / Mathf.Sqrt(2)); //normalizes the vector to prevent player from moving faster diagonally
+        }
+        else
+        {
+            movement = new Vector3(speedDif.x * accelRate.x, 0f, speedDif.z * accelRate.z); //sets players speed to acceleration multiplied by the distance between current speed and target speed.
+        }
+        playerBody.AddForce(movement.x, movement.y, movement.z, ForceMode.Force); //applies the force to the player.
+    }
+
+    private void Jump()
+    { 
+        float force = Data.jumpForce;
+
+		playerBody.AddForce(Vector3.up * force, ForceMode.Impulse);
+    }
+}
